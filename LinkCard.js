f2e01146023c113c60da3d8a424e973fbf519140
Benjahmin Koenigsberg <benjahmin.lakin@gmail.com>
2023-08-16 11:47:34 -0400

    Add files via upload

--- LinkCard.js
+++ LinkCard.js
@@ -1,102 +1,122 @@
-import {useState} from 'react'
+import React from 'react'
+import { useState  } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLink } from "@fortawesome/free-solid-svg-icons";
+import { faC, faLink } from "@fortawesome/free-solid-svg-icons";
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
-//import { v4 as uuidv4 } from "uuid";
-import click from "../game-click.wav";
+import { faCopy } from '@fortawesome/free-solid-svg-icons';
+import { faShareNodes } from '@fortawesome/free-solid-svg-icons';
+
+const LinkCard = ({ category, content, setContent, allLinks }) => {
+
+  function handleContent(e) {}
+
+  const [isFavorite, setIsFavorite] = useState(false);
+  // const [favArray, updateFavArray] = useState([])
+
+  const addToFavorites = () => {
+    if (!category.description) category.description = "  ";
+    const favObj = {
+      name: category.name,
+      link: category.link,
+      type: category.type,
+      category: category.category,
+      description: category.description,
+    };
+    localStorage.setItem(category.name, JSON.stringify(favObj));
+
+    if (!isFavorite) {
+      setIsFavorite(true);
+      toast(`Link added to favorites`, {
+        position: "top-right",
+        autoClose: 1500,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        type: "error",
+      });
+    } else {
+        setIsFavorite(false);
+           localStorage.removeItem(category.name);
+        toast(`Link removed from favorites`, {
+            position: "top-right",
+            autoClose: 1500,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            type: "info",
+        });
+
+        //   const getStorage = () => {
+        //     let favArr = [];
+        //       for (let i = 0; i <= localStorage.length; i++) {
+        //           let favObj = localStorage.getItem(localStorage.key(i));
+        //           if (favObj !== "INFO") favArr.push(JSON.parse(favObj));
+        //           setContent(favArr);
+        //          document.location.reload();
+        //       }
+        //   };
+        //   getStorage();
+      };
+      return;
+        };
 
 
-export const LinkCard = ({ topic }) => {
-
-const audio = new Audio(click);
-
-const [isFavorite, setIsFavorite] = useState(false)
-const [ readMore, setReadMore ] = useState(false)
-const abrieve = topic.description.slice(0,100)
-const [ description, setDescription] = useState(abrieve)
-
-const addToFavorites = () => {
-if(!topic.description) topic.description = '  ';
-const favObj = {
-  name: topic.name,
-  link: topic.link,
-  type: topic.type,
-  category: topic.category,
-  description: topic.description,
-};
-localStorage.setItem(topic.name, JSON.stringify(favObj))
-
-if (!isFavorite) {
-setIsFavorite(true);
-audio.play();
-}
-else {
-  return
-// setIsFavorite(false)
-}
-}
+  function copyLink() {
+    navigator.clipboard.writeText(category.link);
+    toast(`URL copied to clipboard!`, {
+      position: "top-right",
+      autoClose: 2500,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      type: "success",
+    });
+  }
 
   return (
-    <>
-      <div className="w-50 h-auto border p-1 px-3 m-2 shadow-lg cursor-pointer text-center">
-        <h3 className="p-1">{topic.name}</h3>
-
-        {topic.description.length > 100 ? (
-          <>
-            <div>
-              <p>{description}</p>
-              <button
-                className="text-indigo-700 cursor-pointer rajdhani"
-                onClick={() => {
-                  if (!readMore) {
-                    setReadMore((prev) => !prev);
-                    // document.getElementById("description").innerText =
-                    //   topic?.description;
-                    setDescription(topic.description);
-                  } else {
-                    setReadMore((prev) => !prev);
-                    // document.getElementById("description").innerText =
-                    //   topic?.description.slice(0, 100);
-                    // const abrieve = topic.description.slice(1, 100);
-                    setDescription(abrieve);
-                  }
-                }}>
-                {!readMore ? "...read more" : "read less"}
+    <div className="LinkCard-container" id={category.name}>
+      <div className="LinkCard-inner-container">
+        <p className="LinkCard-emoji">{category?.emoji}</p>
+        <p className="LinkCard-name">{category?.name}</p>
+        <p className="LinkCard-description">{category?.description}</p>
+        <p className="LinkCard-category">{category?.category}</p>
+        <div className="LinkCard-buttons-container">
+          <a href={category?.link} id="link">
+            <div className="LinkCard-button-background">
+              <button>
+                <FontAwesomeIcon className="LinkCard-button" icon={faLink} />
               </button>
             </div>
-          </>
-        ) : (
-          <p>{topic.description}</p>
-        )}
-
-        <p className="">{topic?.category}</p>
-        <p className="">
-          {topic.autor ? "By" : ""} {topic?.author}
-        </p>
-
-        {topic.link ? (
-          <div className="flex justify-between px-2 pb-2">
-            <a href={topic?.link} target="_blank">
-              <button className="bg-gray-200 text-black font-bold px-8 rounded m-1">
-                <FontAwesomeIcon icon={faLink} />
-              </button>
-            </a>
-            <div>
-              <button
-                className="bg-gray-200 text-black font-bold px-2 rounded m-1"
-                onClick={addToFavorites}>
-                {!isFavorite ? (
-                  <FontAwesomeIcon icon={faHeart} />
-                ) : (
-                  <FontAwesomeIcon icon={faHeart} className="text-red-500" />
-                )}
-              </button>
-            </div>
+          </a>
+          <div className="LinkCard-button-background" onClick={addToFavorites}>
+            <button>
+              <FontAwesomeIcon className={ isFavorite ? "favorite LinkCard-button" : "LinkCard-button" }  icon={faHeart} />
+            </button>
           </div>
-        ) : (
-          ""
-        )}
+          <div className="LinkCard-button-background">
+            <button onClick={copyLink}>
+              <FontAwesomeIcon
+                className="LinkCard-button"
+                icon={faCopy}
+              />
+            </button>
+            <ToastContainer />
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   );
-}
+};
+
+export default LinkCard
